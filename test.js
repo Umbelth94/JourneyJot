443ac0dfc35ecee705dce0cea54b729939a46e93
Travis Umbel <Umbelth94@gmail.com>
2024-03-05 20:59:30 -0600

    husky/prettier test 3

--- test.js
+++ test.js
@@ -4,3 +4,10 @@ console.log(prettierTesting);
 var evenMoreTesting = "Laa la laaa";
 
 console.log(evenMoreTesting);
+
+function randomFunction() {
+    console.log("hey good job nerd");
+    return;
+}
+
+randomFunction();
