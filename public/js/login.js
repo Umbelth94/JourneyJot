9202af844cd24d6441df8d7efa5be354ae264214
Travis Umbel <Umbelth94@gmail.com>
2024-03-11 21:44:09 -0500

    removed profile_pic from user model since it was being real wonky.

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
 
         if (response.ok) {
             // If successful, redirect the browser to the dashboard page
-            document.location.replace("/homepage");
+            document.location.replace("/");
         } else {
             alert(response.statusText);
         }
@@ -31,14 +31,15 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector("#password-signup").value.trim();
 
     if (username && email && password) {
-        const response = await fetch("/api/user", {
+        const response = await fetch("/api/user/signup", {
             method: "POST",
             body: JSON.stringify({ username, email, password }),
             headers: { "Content-Type": "application/json" },
         });
 
         if (response.ok) {
-            document.location.replace("/homepage");
+            alert("account signed up!");
+            document.location.replace("/");
         } else {
             alert(response.statusText);
         }
