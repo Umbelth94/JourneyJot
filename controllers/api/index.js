4c0a1f456a6983747e19be0d2c0a414a56ba8589
Stroyer210 <sergiosthiwar@gmail.com>
2024-03-14 03:37:11 -0500

    adding code in the journey route plus created the seeds

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes.js");
+const journeyRoutes = require("./journeyRoutes");
 
 //Route for any user related API data (logging in, logging out)
 router.use("/user", userRoutes);
+router.use("/journeys", journeyRoutes);
 
 module.exports = router;
