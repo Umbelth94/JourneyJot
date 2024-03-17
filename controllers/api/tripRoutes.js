d5df4f704e7a4458c814d7557d1d65ac662c684c
Travis Umbel <Umbelth94@gmail.com>
2024-03-17 16:25:47 -0500

    Added handlebars page and modified "/trips" route from the homepage

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -29,20 +29,20 @@ router.post("/save", withAuth, async (req, res) => {
 });
 
 // Route to fetch user's trips
-router.get("/trips", async (req, res) => {
-    try {
-        const userId = req.user.id;
+// router.get("/", async (req, res) => {
+//     try {
+//         const userId = req.user.id;
 
-        // Fetch trips data for the logged-in user
-        const trip = await Trip.findAll({
-            where: { userId: userId },
-        });
+//         // Fetch trips data for the logged-in user
+//         const trip = await Trip.findAll({
+//             where: { userId: userId },
+//         });
 
-        res.render("trips", { trip });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
+//         res.render("trips", { trip });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
 
 module.exports = router;
